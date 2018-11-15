9d883887ffac41f5b4f90eefd5fb4fbd3ce4e0e9
chaichai <1224816105@qq.com>
2018-11-15 13:08:42 +0800

    jieba测试

--- pandastestiris.py
+++ pandastestiris.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
 import pandas as pd
-import requests
+import request
 
-PATH = r'./data/'
-r = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris'
+PATH = r'E:\deep learning\code\data'
+r = request.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris'
                  '/iris.data')
-with open(PATH + 'iris.data', 'w+') as f:
+with open(PATH + 'iris.data', 'w') as f:
     f.write(r.text)
 
 os.chdir(PATH)
 
 # add title to csv files
-df = pd.read_csv(PATH + 'iris.data', names=['sepal length', 'sepal width',
-                                            'petal length', 'petal width', 'class'])
+df = pd.read_csv(PATH + 'iris.data',
+                 names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'],
+                 engine='python')
 
-df.head()
+print(df.head())
+print(df['class'].unique())
+
