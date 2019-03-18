a3377e42dedf77579d3481caf7458c0c13aadf9e
chaichai <1224816105@qq.com>
2019-03-18 18:03:58 +0800

    MultinomialNB高斯贝叶斯分类器及其α值的影响

--- machine learning/algorithmtest/GaussianNB.py
+++ machine learning/algorithmtest/GaussianNB.py
@@ -11,6 +11,20 @@ def load_data():
 
 """
 高斯贝叶斯分类器
+__init__(self, priors=None, var_smoothing=1e-9):
+        self.priors = priors
+        self.var_smoothing = var_smoothing
+    属性：
+    class_prior_:一个数组，形状为(n_classes,)，是每个类别的概率
+    class_count_:一个数组，形状为(n_classes,),是每个类别包含的训练样本数量
+    theta_:一个数组，形状为(n_classes,),是每个类别上每个特征的均值
+    sigma_:一个数组，形状为(n_classes,)，是每个类别上的每个特征的标准差
+    方法：
+    fit()：训练模型
+    partial_fit():追加训练模型
+    predict（）：预测
+    predict_log_proba():返回一个数组，数组元素依次为各个类别的概率值
+    score():预测准确度
 """
 
 
