bbf99c79e5125b12bf886f640e3729ae4d89d514
chaichai <1224816105@qq.com>
2019-03-25 16:41:08 +0800

    Gaussian、Median、Bilateral Filtering 滤波

--- opencvmoduel/smoothing images.py
+++ opencvmoduel/smoothing images.py
@@ -25,18 +25,41 @@ filter2D
 # plt.show()
 
 # image Blurring(Smoothing)
-"""
-average()
-"""
-blur = cv2.blur(img, (5, 5))
+
+# blur1 = cv2.blur(img, (5, 5))  # AVERAGE
+# blur2 = cv2.GaussianBlur(img, (5, 5), 0)  # Gaussian Blurring
+# median = cv2.medianBlur(img, 5)  # Median 中值模糊
+# plt.subplot(221)
+# plt.imshow(img)
+# plt.title("Original")
+# plt.xticks([])
+# plt.yticks([])
+# plt.subplot(222)
+# plt.imshow(blur1)
+# plt.title("Average")
+# plt.xticks([])
+# plt.yticks([])
+# plt.subplot(223)
+# plt.imshow(blur2)
+# plt.title("Gaussian")
+# plt.xticks([])
+# plt.yticks([])
+# plt.subplot(224)
+# plt.imshow(median)
+# plt.title("Median")
+# plt.xticks([])
+# plt.yticks([])
+# plt.show()
+
+# Bilateral Filtering 双边滤波 可消除噪音并保持边缘
+blur = cv2.bilateralFilter(img, 9, 75, 75)
 plt.subplot(121)
 plt.imshow(img)
-plt.title("Original")
-plt.xticks([])
-plt.yticks([])
+plt.title("before")
 plt.subplot(122)
 plt.imshow(blur)
-plt.title("Blurred")
-plt.xticks([])
-plt.yticks([])
+plt.title("after")
 plt.show()
+
+
+
